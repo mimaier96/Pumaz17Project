b04ed06ef4fc0af9e824a6940137698caed245bc
MaSpecter <Admin@NB15200.mshome.net>
2018-02-27 14:14:08 +0100

    StartViewApprentice FXML GUI + StartViewApprenticeController Class

---
+++ Pumaz17Projekt/src/de/dpma/projekt/view/StartViewApprenticeController.java
@@ -0,0 +1,32 @@
+package de.dpma.projekt.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class StartViewApprenticeController {
+
+	@FXML
+	private Label nameLabel;
+	@FXML
+	private Label birthdayLabel;
+	@FXML
+	private Label adressLabel;
+	@FXML
+	private Label beginOfEmployment;
+	@FXML
+	private Label endOfEmployment;
+	@FXML
+	private Label deploymentLocation;
+	
+	
+
+	// Konstruktor
+	public StartViewApprenticeController() {
+	}
+	
+	@FXML
+	private void handleCreateReportBook() {
+		
+	}
+
+}
