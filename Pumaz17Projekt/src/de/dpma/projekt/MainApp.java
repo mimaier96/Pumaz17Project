89adaac69a39b83cd92f20f71fd0e0757297de81
Azubi <Azubi@DESKTOP-CMS0GTU.mshome.net>
2018-02-27 13:15:50 +0100

    Erstellen von
    void loadScene()
    
    vereinfachen des Load Prozesses

--- Pumaz17Projekt/src/de/dpma/projekt/MainApp.java
+++ Pumaz17Projekt/src/de/dpma/projekt/MainApp.java
@@ -1,16 +1,53 @@
 package de.dpma.projekt;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
+	private Stage primaryStage;
+
 	@Override
 	public void start(Stage primaryStage) {
-		
+
+		this.primaryStage = primaryStage;
+		loadScene("view/LoginWindow.fxml","Anchor");
 	}
 
 	public static void main(String[] args) {
 		launch(args);
+	}
+
+	public void loadScene(String path, String paneType) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource(path));
+			Scene scene;
+
+			switch (paneType) {
+			case "Anchor":
+				scene = new Scene(new AnchorPane((AnchorPane) loader.load()));
+				break;
+			case "Border":
+				scene = new Scene(new BorderPane((BorderPane) loader.load()));
+				break;
+			default:
+				scene = new Scene(new AnchorPane((AnchorPane) loader.load()));
+			}
+
+			this.primaryStage.setScene(scene);
+			this.primaryStage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 }
--- Pumaz17Projekt/src/de/dpma/projekt/models/user/Apprentice.java
+++ Pumaz17Projekt/src/de/dpma/projekt/models/user/Apprentice.java
@@ -2,25 +2,14 @@ package de.dpma.projekt.models.user;
 
 import de.dpma.projekt.models.User;
 
-import java.util.Date;
-
-
 public class Apprentice extends User {
 	
-	private int user_id;
 	private String job;
+	private int user_id;
 	private User instructor;
-	private byte yearOfEmployment;
-	private Date birthday;
-	private int postalCode;
-	private String street;
-	private short housenumber;
-	private String cityPostalCode;
-	private Date employmentBegin;
-	private Date employmentEnd;
-	private String placeOfEmployment;
 	
-
+	
+	boolean irgendwas;
 	
 	public Apprentice(int user_id, User instructor) {
 		this.user_id = user_id;
