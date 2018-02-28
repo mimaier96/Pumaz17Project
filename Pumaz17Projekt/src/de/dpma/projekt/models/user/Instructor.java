f361f7be3795f89acddbcc988ff16a453cbf5bce
Azubi <Azubi@192.168.137.79>
2018-02-28 09:58:05 +0100

    Update

---
+++ Pumaz17Projekt/src/de/dpma/projekt/models/user/Instructor.java
@@ -0,0 +1,17 @@
+package de.dpma.projekt.models.user;
+
+import de.dpma.projekt.models.User;
+
+public class Instructor extends User{
+
+	public Instructor() {
+		
+	}
+
+	
+	public User createApprentice() {
+		Apprentice a = new Apprentice();
+		return 
+		
+	}
+}
