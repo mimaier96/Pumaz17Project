49644a047c5ad15ded4e6240b48709e25bbdea61
MaSpecter <Admin@192.168.137.198>
2018-02-28 12:10:30 +0100

    ApprenticeDao Inteface && ApprenticeDaoImplementation Klasse

---
+++ Pumaz17Projekt/src/de/dpma/projekt/db/ApprenticeDao.java
@@ -0,0 +1,23 @@
+package de.dpma.projekt.db;
+
+
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import de.dpma.projekt.models.user.Apprentice;
+
+public interface ApprenticeDao {
+	
+public Apprentice insertApprentice(Apprentice apprentice) throws SQLException;
+	
+	public ArrayList<Apprentice> insertApprentice(ArrayList<Apprentice> apprentices);
+
+	public boolean updateApprentice(Apprentice apprentice);
+
+	
+	public boolean deleteApprentice(int userId);
+	
+	public ArrayList<Apprentice> allApprentices();
+
+}
