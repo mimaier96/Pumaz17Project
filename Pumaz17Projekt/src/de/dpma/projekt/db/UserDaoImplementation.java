40dde87c519d0f13fc3dd2291896fd99faa45ac4
MaSpecter <Admin@192.168.137.198>
2018-02-28 14:20:21 +0100

    Apprentice Variablen + Getter und setter hinzugefügt

--- Pumaz17Projekt/src/de/dpma/projekt/db/UserDaoImplementation.java
+++ Pumaz17Projekt/src/de/dpma/projekt/db/UserDaoImplementation.java
@@ -1,47 +1,63 @@
 package de.dpma.projekt.db;
 
 
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import de.dpma.projekt.models.user.Apprentice;
+import de.dpma.projekt.models.user.Apprentice;;
 
-public class ApprenticeDaoImplementation implements ApprenticeDao {
+public class UserDaoImplementation implements UserDao {
 
 	private Connection con = DatabaseConnection.getInstance();
 
+	/* Dieser Boolean wird dazu benutzt, ob zusätzliche Felder für den Azubi
+	 angezeigt werden und somit ausgefüllt werden müssen, oder nicht */
+	boolean userIsApprentice = false;
+
+	/**
+	 * SQL Statement Strings
+	 * 
+	 * @author MaSpecter
+	 */
+
 	private final static String PREPARED_INSERT_USER = "INSERT INTO user (firstname, lastname, username, password, role, email) VALUES (?,?,?,Anfang12,?,?)";
 	private final static String PREPARED_SELECT_USER = "SELECT id, firstname, lastname, username, password, role, email FROM user WHERE id = ?";
-	private final static String PREPARED_INSERT_APPRENTICE = "";
+	private final static String PREPARED_INSERT_APPRENTICEDATA = "INSERT INTO apprentice (job, instructor, YearOfEmployment, birthday, street, house number, postal code, city, location of deployment, Begin of apprenticeship, End of apprenticeship) VALUES(?,?,?,?,?,?,?,?,?,?,?)";
 
 	@Override
 	public Apprentice insertApprentice(Apprentice apprentice) throws SQLException {
-		
+
 		/**
-		 * Daten in Datenbank schreiben
-		 * Reihenfolge: firstname, lastname, username, password, role, email
+		 * Daten in Datenbank schreiben Reihenfolge: firstname, lastname, username,
+		 * password, role, email
 		 * 
 		 * @author MaSpecter
 		 */
-		
+
 		String generatedColumns[] = { "ID" };
 		PreparedStatement preparedApprenticeInsert = con.prepareStatement(PREPARED_INSERT_USER, generatedColumns);
-		
+
 		preparedApprenticeInsert.setString(1, apprentice.getFirstname());
 		preparedApprenticeInsert.setString(2, apprentice.getLastname());
 		preparedApprenticeInsert.setString(3, apprentice.getUsername());
-//		preparedApprenticeInsert.setString(4, apprentice.getPassword());
+		// preparedApprenticeInsert.setString(4, apprentice.getPassword());
 		preparedApprenticeInsert.setString(5, apprentice.getRole());
 		preparedApprenticeInsert.setString(6, apprentice.getEmail());
+		
+		//	NUR bei Azubi
+		
+		preparedApprenticeInsert.setString(7, apprentice.getJob());
+		preparedApprenticeInsert.setString(8, (apprentice.getInstructor().getFirstname()+" "+apprentice.getInstructor().getLastname()));
+		preparedApprenticeInsert.setString(9, x);
+		
 
 		preparedApprenticeInsert.execute();
 
 		ResultSet resSet = preparedApprenticeInsert.getGeneratedKeys();
-		
+
 		if (resSet.next()) {
 			int id = (int) resSet.getLong(1);
 			apprentice.setId(id);
--- Pumaz17Projekt/src/de/dpma/projekt/models/user/Apprentice.java
+++ Pumaz17Projekt/src/de/dpma/projekt/models/user/Apprentice.java
@@ -1,12 +1,26 @@
 package de.dpma.projekt.models.user;
 
+
+import java.time.Year;
+import java.util.Date;
+
 import de.dpma.projekt.models.User;
 
 public class Apprentice extends User {
 
-	private String job;
+	private String job; //Ausbildungsberuf
 	private int user_id;
-	private User instructor;
+	private User instructor; //Ausbildungsleiter/in
+	
+	private Year yearOfEmployment; //Ausbildungsjahr, in dem sich der Azubi befindet
+	private Date apprenticeBirthday; //Geburtstag
+	private String adressStreetApprentice; //Adresse: Straße
+	private int adressHouseNumberApprentice; //Adresse: Hausnummer
+	private int adressPostalCode; //Adresse: Postleitzahl
+	private String adressCity; //Adresse: Stadt zur Postleitzahl
+	private String locationOfDeployment; //ausbildende Behörde (München/Jena)
+	private Date BeginOfApprenticeship; //Ausbildungsbeginn
+	private Date EndOfApprenticeship; //Ausbildungsende
 
 
 	public Apprentice(int id, String firstname, String lastname, String username, String password, String role, String email, User instructor) {
@@ -15,4 +29,113 @@ public class Apprentice extends User {
 		this.instructor = instructor;
 	}
 
+
+	public String getJob() {
+		return job;
+	}
+
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+
+	public User getInstructor() {
+		return instructor;
+	}
+
+
+	public void setInstructor(User instructor) {
+		this.instructor = instructor;
+	}
+
+
+	public Year getYearOfEmployment() {
+		return yearOfEmployment;
+	}
+
+
+	public void setYearOfEmployment(Year yearOfEmployment) {
+		this.yearOfEmployment = yearOfEmployment;
+	}
+
+
+	public Date getApprenticeBirthday() {
+		return apprenticeBirthday;
+	}
+
+
+	public void setApprenticeBirthday(Date apprenticeBirthday) {
+		this.apprenticeBirthday = apprenticeBirthday;
+	}
+
+
+	public String getAdressStreetApprentice() {
+		return adressStreetApprentice;
+	}
+
+
+	public void setAdressStreetApprentice(String adressStreetApprentice) {
+		this.adressStreetApprentice = adressStreetApprentice;
+	}
+
+
+	public int getAdressHouseNumberApprentice() {
+		return adressHouseNumberApprentice;
+	}
+
+
+	public void setAdressHouseNumberApprentice(int adressHouseNumberApprentice) {
+		this.adressHouseNumberApprentice = adressHouseNumberApprentice;
+	}
+
+
+	public int getAdressPostalCode() {
+		return adressPostalCode;
+	}
+
+
+	public void setAdressPostalCode(int adressPostalCode) {
+		this.adressPostalCode = adressPostalCode;
+	}
+
+
+	public String getAdressCity() {
+		return adressCity;
+	}
+
+
+	public void setAdressCity(String adressCity) {
+		this.adressCity = adressCity;
+	}
+
+
+	public String getLocationOfDeployment() {
+		return locationOfDeployment;
+	}
+
+
+	public void setLocationOfDeployment(String locationOfDeployment) {
+		this.locationOfDeployment = locationOfDeployment;
+	}
+
+
+	public Date getBeginOfApprenticeship() {
+		return BeginOfApprenticeship;
+	}
+
+
+	public void setBeginOfApprenticeship(Date beginOfApprenticeship) {
+		BeginOfApprenticeship = beginOfApprenticeship;
+	}
+
+
+	public Date getEndOfApprenticeship() {
+		return EndOfApprenticeship;
+	}
+
+
+	public void setEndOfApprenticeship(Date endOfApprenticeship) {
+		EndOfApprenticeship = endOfApprenticeship;
+	}
 }
