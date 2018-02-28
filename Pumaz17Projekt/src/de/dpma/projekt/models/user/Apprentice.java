61c4d7d8e91c2897ba47a2cbb4d900c7b098a126
Luca <Luca@tollerJungerMann.de>
2018-02-28 10:36:46 +0100

    Ein bisscehn Modelliert LELLE

--- Pumaz17Projekt/src/de/dpma/projekt/models/user/Apprentice.java
+++ Pumaz17Projekt/src/de/dpma/projekt/models/user/Apprentice.java
@@ -3,19 +3,16 @@ package de.dpma.projekt.models.user;
 import de.dpma.projekt.models.User;
 
 public class Apprentice extends User {
-	
+
 	private String job;
 	private int user_id;
 	private User instructor;
-	
-	
-	boolean irgendwas;
-	
-	public Apprentice(int user_id, User instructor) {
-		this.user_id = user_id;
+
+
+	public Apprentice(int id, String firstname, String lastname, String username, String password, String role, String email, User instructor) {
+		super(id, firstname, lastname, username, password, role, email);
+		this.user_id = id;
 		this.instructor = instructor;
 	}
-	
-	
 
 }
