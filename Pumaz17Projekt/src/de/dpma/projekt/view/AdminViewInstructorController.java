c1294a4ac66cfa0f21c8b97db480a2304ac6fff9
DPMA2017 <33487075+DPMA2017@users.noreply.github.com>
2018-03-06 08:55:18 +0100

    buttons verlinkung

--- Pumaz17Projekt/src/de/dpma/projekt/view/AdminViewInstructorController.java
+++ Pumaz17Projekt/src/de/dpma/projekt/view/AdminViewInstructorController.java
@@ -9,7 +9,7 @@ public class AdminViewInstructorController {
 
 	@FXML
 	private void initialize() {
-		
+
 	}
 
 	public AdminViewInstructorController() {
@@ -19,12 +19,16 @@ public class AdminViewInstructorController {
 	@FXML
 	private void handleAdminPermissionsButton() {
 
-
 	}
-	
+
 	@FXML
 	private void handleAddUser() {
 		mainApp.loadScene("view/AdminViewAddUser.fxml", "Benutzer hinzufügen");
+	}
+
+	@FXML
+	private void backToInstructorView() {
+		mainApp.loadScene("view/StartViewInstructor.fxml", "Übersicht");
 	}
 
 	public static void setMainApp(MainApp mainApp) {
---
+++ Pumaz17Projekt/src/de/dpma/projekt/view/ReportBookWeeklyController.java
@@ -0,0 +1,20 @@
+package de.dpma.projekt.view;
+
+import de.dpma.projekt.MainApp;
+import javafx.fxml.FXML;
+
+public class ReportBookWeeklyController {
+
+	public static MainApp mainApp;
+
+	@FXML
+	private void backToApprenticeView() {
+		mainApp.loadScene("view/StartViewApprentice.fxml", "Übersicht");
+	}
+
+	public static void setMainApp(MainApp mainApp) {
+		AdminViewInstructorController.mainApp = mainApp;
+
+	}
+
+}
