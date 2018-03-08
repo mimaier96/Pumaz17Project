7d5509c8ade92d84f7194c246dfd79e8c528d668
Luca <Luca@tollerJungerMann.de>
2018-03-08 09:10:48 +0100

    MaSpecter - Changing from den tollen Things

--- Pumaz17Projekt/src/de/dpma/projekt/view/AdminViewAddUserController.java
+++ Pumaz17Projekt/src/de/dpma/projekt/view/AdminViewAddUserController.java
@@ -121,168 +121,168 @@ public class AdminViewAddUserController {
 		this.dialogStage = dialogStage;
 	}
 
-	private boolean inputIsValid() {
-		
-		log.info("-->Starte: inputIsValid -- Datenüberprüfung");
-
-		String errorMessage = "";
-
-		if (firstNameField.getText() == null || firstNameField.getText().length() == 0) {
-			errorMessage += "Kein gültiger Vorname!\n";
-		}
-
-		if (lastNameField.getText() == null || lastNameField.getText().length() == 0) {
-			errorMessage += "Kein gültiger Nachname!\n";
-		}
-
-		if (userNameField.getText() == null || userNameField.getText().length() == 0) {
-			errorMessage += "Kein gültiger Benutzername!\n";
-		}
-
-		// hier nicht nötig
-		
-//		if (passwordField.getText() == null || passwordField.getText().length() == 0) {
-//			errorMessage += "Kein gültiges Passwort!\n";
+//	private boolean inputIsValid() {
+//		
+//		log.info("-->Starte: inputIsValid -- Datenüberprüfung");
+//
+//		String errorMessage = "";
+//
+//		if (firstNameField.getText() == null || firstNameField.getText().length() == 0) {
+//			errorMessage += "Kein gültiger Vorname!\n";
 //		}
-
-		// Variablen zur Überprüfung, ob Email '@ und .' enthält
-		String eMCs = "@";
-		CharSequence emailMustChars = eMCs;
-
-		/**
-		 * Überprüft, ob Email '@ und .' enthält
-		 * 
-		 * @author MaSpecter
-		 */
-
-		boolean isEmailCorrect = emailField.getText().contains(emailMustChars);
-		if (emailField.getText() == null || emailField.getText().length() == 0) {
-			errorMessage += "Bitte geben Sie eine E-Mail Adresse an!\\n";
-		} else if (isEmailCorrect == false) {
-			errorMessage += "Keine gültige E-Mail Adresse!\\n";
-		}
-
-		// Nur für Azubis
-
-		if (yearOfEmploymentField.getText() == null || yearOfEmploymentField.getText().length() == 0) {
-			errorMessage += "Kein gültiges Ausbildungsjahr!\\n";
-		} else {
-			// String Eingabe wird auf eine Ganzzahl überprüft
-			try {
-				Integer.parseInt(yearOfEmploymentField.getText());
-			} catch (NumberFormatException e) {
-				errorMessage += "Kein gültiges Ausbildungsjahr!\n";
-			}
-		}
-
-//		if (birthdayField.getText() == null || birthdayField.getText().length() == 0) {
-//			errorMessage += "Kein gültiges Geburtsdatum! Format: TT.MM.JJJJ\n";
+//
+//		if (lastNameField.getText() == null || lastNameField.getText().length() == 0) {
+//			errorMessage += "Kein gültiger Nachname!\n";
+//		}
+//
+//		if (userNameField.getText() == null || userNameField.getText().length() == 0) {
+//			errorMessage += "Kein gültiger Benutzername!\n";
+//		}
+//
+//		// hier nicht nötig
+//		
+////		if (passwordField.getText() == null || passwordField.getText().length() == 0) {
+////			errorMessage += "Kein gültiges Passwort!\n";
+////		}
+//
+//		// Variablen zur Überprüfung, ob Email '@ und .' enthält
+//		String eMCs = "@";
+//		CharSequence emailMustChars = eMCs;
+//
+//		/**
+//		 * Überprüft, ob Email '@ und .' enthält
+//		 * 
+//		 * Maracus
+//		 */
+//
+//		boolean isEmailCorrect = emailField.getText().contains(emailMustChars);
+//		if (emailField.getText() == null || emailField.getText().length() == 0) {
+//			errorMessage += "Bitte geben Sie eine E-Mail Adresse an!\\n";
+//		} else if (isEmailCorrect == false) {
+//			errorMessage += "Keine gültige E-Mail Adresse!\\n";
+//		}
+//
+//		// Nur für Azubis
+//
+//		if (yearOfEmploymentField.getText() == null || yearOfEmploymentField.getText().length() == 0) {
+//			errorMessage += "Kein gültiges Ausbildungsjahr!\\n";
 //		} else {
-//			if (!DateUtil.validDate(birthdayField.getText())) {
-//				errorMessage += "Kein gültiges Geburtsdatum! Format: TT.MM.JJJJ\n";
-//			}
-//		}
-
-//		if (streetField.getText() == null || streetField.getText().length() == 0) {
-//			errorMessage += "Keine gültiger Straßenname!\\n";
-//		}
-//		if (houseNumberField.getText() == null || houseNumberField.getText().length() == 0) {
-//			errorMessage += "Keine gültige Hausnummer!\\n";
-//		} else {
+//			// String Eingabe wird auf eine Ganzzahl überprüft
 //			try {
-//				Integer.parseInt(houseNumberField.getText());
+//				Integer.parseInt(yearOfEmploymentField.getText());
 //			} catch (NumberFormatException e) {
-//				errorMessage += "Keine gültige Hausnummer!\\n";
+//				errorMessage += "Kein gültiges Ausbildungsjahr!\n";
 //			}
 //		}
 //
-//		// Überprüft, ob die Eingabe eine fünfstellige Ganzzahl ist
-//		if (postalCodeField.getText() == null || postalCodeField.getText().length() != 5) {
-//			errorMessage += "Bitte geben Sie eine Postleitzahl ein!\\n";
-//		} else {
-//			try {
-//				/**
-//				 * Überprüft den gültigen Postleitzahlen-Bereich von Deutschland
-//				 * 
-//				 * @author MaSpecter
-//				 */
-//				int plzCheck = Integer.parseInt(postalCodeField.getText());
-//				if (plzCheck < 01000 || plzCheck > 99998) {
-//					errorMessage += "Keine gültige Postleizahl!\\n";
-//				}
-//			} catch (NumberFormatException e) {
-//				errorMessage += "Keine gültige Postleitzahl!\\n";
-//			}
-//		}
+////		if (birthdayField.getText() == null || birthdayField.getText().length() == 0) {
+////			errorMessage += "Kein gültiges Geburtsdatum! Format: TT.MM.JJJJ\n";
+////		} else {
+////			if (!DateUtil.validDate(birthdayField.getText())) {
+////				errorMessage += "Kein gültiges Geburtsdatum! Format: TT.MM.JJJJ\n";
+////			}
+////		}
 //
-//		if (cityField.getText() == null || cityField.getText().length() == 0) {
-//			errorMessage += "Keine gültige Stadt!\\n";
-//		}
-
-//		if (beginOfApprenticeshipField.getText() == null || beginOfApprenticeshipField.getText().length() == 0) {
-//			errorMessage += "Kein gültiges Datum für den Ausbildungsbeginn! Format: TT.MM.JJJJ\\n";
-//		} else {
-//			if (!DateUtil.validDate(beginOfApprenticeshipField.getText())) {
-//				errorMessage += "Kein gültiges Datum für den Ausbildungsbeginn! Format: TT.MM.JJJJ\\n";
-//			}
-//		}
+////		if (streetField.getText() == null || streetField.getText().length() == 0) {
+////			errorMessage += "Keine gültiger Straßenname!\\n";
+////		}
+////		if (houseNumberField.getText() == null || houseNumberField.getText().length() == 0) {
+////			errorMessage += "Keine gültige Hausnummer!\\n";
+////		} else {
+////			try {
+////				Integer.parseInt(houseNumberField.getText());
+////			} catch (NumberFormatException e) {
+////				errorMessage += "Keine gültige Hausnummer!\\n";
+////			}
+////		}
+////
+////		// Überprüft, ob die Eingabe eine fünfstellige Ganzzahl ist
+////		if (postalCodeField.getText() == null || postalCodeField.getText().length() != 5) {
+////			errorMessage += "Bitte geben Sie eine Postleitzahl ein!\\n";
+////		} else {
+////			try {
+////				/**
+////				 * Überprüft den gültigen Postleitzahlen-Bereich von Deutschland
+////				 * 
+////				 * Maracus
+////				 */
+////				int plzCheck = Integer.parseInt(postalCodeField.getText());
+////				if (plzCheck < 01000 || plzCheck > 99998) {
+////					errorMessage += "Keine gültige Postleizahl!\\n";
+////				}
+////			} catch (NumberFormatException e) {
+////				errorMessage += "Keine gültige Postleitzahl!\\n";
+////			}
+////		}
+////
+////		if (cityField.getText() == null || cityField.getText().length() == 0) {
+////			errorMessage += "Keine gültige Stadt!\\n";
+////		}
 //
-//		if (endOfApprenticeshipField.getText() == null || endOfApprenticeshipField.getText().length() == 0) {
-//			errorMessage += "Kein gültiges Datum für das Ausbildungsende! Format: TT.MM.JJJJ\\n";
+////		if (beginOfApprenticeshipField.getText() == null || beginOfApprenticeshipField.getText().length() == 0) {
+////			errorMessage += "Kein gültiges Datum für den Ausbildungsbeginn! Format: TT.MM.JJJJ\\n";
+////		} else {
+////			if (!DateUtil.validDate(beginOfApprenticeshipField.getText())) {
+////				errorMessage += "Kein gültiges Datum für den Ausbildungsbeginn! Format: TT.MM.JJJJ\\n";
+////			}
+////		}
+////
+////		if (endOfApprenticeshipField.getText() == null || endOfApprenticeshipField.getText().length() == 0) {
+////			errorMessage += "Kein gültiges Datum für das Ausbildungsende! Format: TT.MM.JJJJ\\n";
+////		} else {
+////			if (!DateUtil.validDate(endOfApprenticeshipField.getText())) {
+////				errorMessage += "Kein gültiges Datum für das Ausbildungsende! Format: TT.MM.JJJJ\\n";
+////			}
+////		}
+//
+//		if (errorMessage.length() == 0) {
+//			log.info("-->Beende: inputIsValid -- Datenüberprüfung");
+//			return true;
 //		} else {
-//			if (!DateUtil.validDate(endOfApprenticeshipField.getText())) {
-//				errorMessage += "Kein gültiges Datum für das Ausbildungsende! Format: TT.MM.JJJJ\\n";
-//			}
+//
+//			// Zeige Fehlermeldung
+//			Alert alert = new Alert(AlertType.ERROR);
+//			alert.initOwner(dialogStage);
+//			alert.setTitle("Falsche oder fehldene Eingaben!");
+//			alert.setHeaderText("Bitte Eingaben korrigieren!");
+//			alert.setContentText(errorMessage);
+//
+//			alert.showAndWait();
+//			
+//			log.info("-->Beende: inputIsValid -- Datenüberprüfung");
+//
+//			return false;
 //		}
-
-		if (errorMessage.length() == 0) {
-			log.info("-->Beende: inputIsValid -- Datenüberprüfung");
-			return true;
-		} else {
-
-			// Zeige Fehlermeldung
-			Alert alert = new Alert(AlertType.ERROR);
-			alert.initOwner(dialogStage);
-			alert.setTitle("Falsche oder fehldene Eingaben!");
-			alert.setHeaderText("Bitte Eingaben korrigieren!");
-			alert.setContentText(errorMessage);
-
-			alert.showAndWait();
-			
-			log.info("-->Beende: inputIsValid -- Datenüberprüfung");
-
-			return false;
-		}
-		
-	}
+//		
+//	}
 
 
 	/**
 	 * Wird aufgerufen, wenn der user auf speichern klickt
 	 * 
-	 * @author MaSpecter
+	 * Maracus
 	 */
 
 	@FXML
 	private void handleSaveAddUser() {
-		if (inputIsValid()) {
+//		if (inputIsValid()) {
 
 			//Wird später aus der db bezogen
 			
 			
 			Date d = new Date(2001,2,30);
-			System.out.println("dadw"+firstNameField.getText()+ lastNameField.getText()+ userNameField.getText()+"Lel"+ roleComboBox.getValue()+ emailField.getText());
-//			if(jobComboBox.getValue().equals("")) {
-				System.out.println(firstNameField.getText()+ lastNameField.getText()+ userNameField.getText()+"Lel"+ roleComboBox.getValue()+ emailField.getText());
-				System.out.println(d);
-				System.out.println(mainApp.i.getFirstnameS());
+//			System.out.println("dadw"+firstNameField.getText()+ lastNameField.getText()+ userNameField.getText()+"Lel"+ roleComboBox.getValue()+ emailField.getText());
+////			if(jobComboBox.getValue().equals("")) {
+//				System.out.println(firstNameField.getText()+ lastNameField.getText()+ userNameField.getText()+"Lel"+ roleComboBox.getValue()+ emailField.getText());
+//				System.out.println(d);
+//				System.out.println(mainApp.i.getFirstnameS());
 //				mainApp.i.createApprentice(firstNameField.getText(), lastNameField.getText(), userNameField.getText(), "Lel", roleComboBox.getValue(), emailField.getText(), 1, "Dieter", 2018, d, streetField.getText(), 3, 81669, cityField.getText(),"DEPLOYMENT" ,d, d);
 
 				mainApp.i.createApprentice("THorste", "OTto", "OTTOWWWO", "Lel","adawda", "kalus@adwd", 1, "Dieter", 2018, d, "adawdad", 3, 81669, "adawda","DEPLOYMENT" ,d, d);//			}
 			addClicked = true;
 			dialogStage.close();
 		}
-	}
+//	}
 
 	@FXML
 	private void handleCancel() {
